04376a60f86064d42049031612eda62b4fdce75a
Tanaphon Auanhinkong <a.tanaphon@gmail.com>
2018-06-30 01:00:11 +0700

    add navigator

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import Layout from './components/Layout/Layout'
+import Layout from './hoc/Layout/Layout'
 import BurgurBuilder from './containers/BurgurBuilder/BurgurBuilder'
 
 class App extends Component {
