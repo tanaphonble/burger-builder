c18e4ea00303dbb8783663272d0e356217b08b46
Tanaphon Auanhinkong <a.tanaphon@gmail.com>
2018-07-01 18:05:32 +0700

    add contact data input

--- src/containers/BurgurBuilder/BurgurBuilder.js
+++ src/containers/BurgurBuilder/BurgurBuilder.js
@@ -103,27 +103,20 @@ class BurgurBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    this.setState({
-      loading: true
-    })
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Max',
-        address: {
-          street: 'st1',
-          zipCode: '12033',
-          country: 'Thailand'
-        },
-        email: 'tets@tee.com'
-      },
-      deliveryMethod: 'fastest'
+    const queryParams = []
+    for (let i in this.state.ingredients) {
+      queryParams.push(
+        encodeURIComponent(i) +
+          '=' +
+          encodeURIComponent(this.state.ingredients[i])
+      )
     }
-    axios
-      .post('/order.json', order)
-      .then((response) => this.setState({ loading: false, purchasing: false }))
-      .catch((error) => this.setState({ loading: false, purchasing: false }))
+    const queryString = queryParams.join('&')
+
+    this.props.history.push({
+      pathname: '/checkout',
+      search: '?' + queryString
+    })
   }
 
   render() {
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
+import ContactData from '../Checkout/ContactData/ContactData'
 
 class Checkout extends Component {
   state = {
@@ -8,14 +10,39 @@ class Checkout extends Component {
       meat: 1,
       bacon: 1,
       salad: 1,
-      cheese:1
+      cheese: 1
     }
+  }
+
+  componentDidMount() {
+    const query = new URLSearchParams(this.props.location.search)
+    const ingredients = {}
+
+    for (let param of query.entries()) {
+      ingredients[param[0]] = +param[1]
+    }
+  }
+
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack()
+  }
+
+  checkoutContinuedHandler = () => {
+    this.props.history.replace('/checkout/contact-data')
   }
 
   render() {
     return (
       <div>
-        <CheckoutSummary ingredients={this.state.ingredients} />
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
+          checkoutCancelled={this.checkoutCancelledHandler}
+          checkoutContinued={this.checkoutContinuedHandler}
+        />
+        <Route
+          path={this.props.match.path + '/contact-data'}
+          component={ContactData}
+        />
       </div>
     )
   }
